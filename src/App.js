d3ae263a556725ca0e5f853d091044d053449afc
SemaElikkaya <elikkayas@gmail.com>
2024-09-13 16:45:08 +0300

    loginview added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate, useLocation } from 'react-router-dom';
 import Header from './components/Header';
 import BottomTabNavigator from './components/BottomTabNavigator';
 import Story from './components/Story';
@@ -7,28 +7,37 @@ import HomeView from './views/HomeView';
 import MenuView from './views/MenuView';
 import SettingsView from './views/SettingsView';
 import QRView from './views/QRView';
+import LoginView from './views/LoginView';
 
-function AppContent() {
+// Protected Route Component
+function ProtectedRoute({ isAuthenticated, children }) {
+  return isAuthenticated ? children : <Navigate to="/login" />;
+}
+
+function AppContent({ isAuthenticated, setIsAuthenticated }) {
   const location = useLocation();
   return (
     <>
-      {location.pathname !== '/story' && <Header />}
+      {location.pathname !== '/story' && location.pathname !== '/login' && <Header />}
       <Routes>
-        <Route path="/" element={<HomeView />} />
-        <Route path="/menu" element={<MenuView />} /> 
-        <Route path="/settings" element={<SettingsView />} />
-        <Route path="/story" element={<Story />} /> 
-        <Route path="/qr" element={<QRView />} />
+        <Route path="/login" element={<LoginView setIsAuthenticated={setIsAuthenticated} />} />
+        <Route path="/" element={<ProtectedRoute isAuthenticated={isAuthenticated}><HomeView /></ProtectedRoute>} />
+        <Route path="/menu" element={<ProtectedRoute isAuthenticated={isAuthenticated}><MenuView /></ProtectedRoute>} />
+        <Route path="/settings" element={<ProtectedRoute isAuthenticated={isAuthenticated}><SettingsView /></ProtectedRoute>} />
+        <Route path="/story" element={<Story />} />
+        <Route path="/qr" element={<ProtectedRoute isAuthenticated={isAuthenticated}><QRView /></ProtectedRoute>} />
       </Routes>
-      {location.pathname !== '/story' && <BottomTabNavigator />}
+      {location.pathname !== '/story' && location.pathname !== '/login' && <BottomTabNavigator />}
     </>
   );
 }
 
 function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false); // Kullanıcı giriş durumu
+
   return (
     <Router>
-      <AppContent />
+      <AppContent isAuthenticated={isAuthenticated} setIsAuthenticated={setIsAuthenticated} />
     </Router>
   );
 }
